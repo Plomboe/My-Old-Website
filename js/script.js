5cac66eec3a2a22d165929ac34d88244cc61343a
Plomboe <m.tareq.karimi@gmail.com>
2017-09-21 23:34:27 -0400

    Script 2.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-//document.write("Hello world!");
+$(function() {
 
-//alert("This is an alert")
+		//Hide other options when one of them is clicked
+    $("#aboutMesvg").on("click", function() {
+			$("#education").removeClass("in");
+			$("#experience").removeClass("in");
+			$("#projects").removeClass("in");
+    });
+
+		$("#educationsvg").on("click", function() {
+			$("#aboutMe").removeClass("in");
+			$("#experience").removeClass("in");
+			$("#projects").removeClass("in");
+    });
+
+		$("#experiencesvg").on("click", function() {
+			$("#aboutMe").removeClass("in");			
+			$("#education").removeClass("in");
+			$("#projects").removeClass("in");
+    });
+
+		$("#projectssvg").on("click", function() {
+			$("#aboutMe").removeClass("in");
+			$("#education").removeClass("in");
+			$("#experience").removeClass("in");		
+    });
+
+});
