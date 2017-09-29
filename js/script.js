c38bc33de14c35ede4a4f110c6b51c92c947ca11
Plomboe <m.tareq.karimi@gmail.com>
2017-09-29 11:48:38 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
 $(function() {
 
-		//Hide other options when one of them is clicked
-    $("#aboutMesvg").on("click", function() {
-			$("#education").removeClass("in");
-			$("#experience").removeClass("in");
-			$("#projects").removeClass("in");
-    });
+		//Take users to various links
+		$("#githubIcon").on("click", function() {
+			window.location.replace("https://github.com/Plomboe");
+		});
+		$("#linkedinIcon").on("click", function() {
+			window.location.replace("https://www.linkedin.com/in/tareq-karimi/");
+		});
+		$("#googleIcon").on("click", function() {
+			window.location.replace("https://plus.google.com/110881320959077166810");
+		});
+	    //Hide other options when one of them is clicked
+	    $("#aboutMesvg").on("click", function() {
+				$("#education").removeClass("in");
+				$("#experience").removeClass("in");
+				$("#projects").removeClass("in");
+	    });
 
-		$("#educationsvg").on("click", function() {
-			$("#aboutMe").removeClass("in");
-			$("#experience").removeClass("in");
-			$("#projects").removeClass("in");
-    });
+			$("#educationsvg").on("click", function() {
+				$("#aboutMe").removeClass("in");
+				$("#experience").removeClass("in");
+				$("#projects").removeClass("in");
+	    });
 
-		$("#experiencesvg").on("click", function() {
-			$("#aboutMe").removeClass("in");			
-			$("#education").removeClass("in");
-			$("#projects").removeClass("in");
-    });
+			$("#experiencesvg").on("click", function() {
+				$("#aboutMe").removeClass("in");			
+				$("#education").removeClass("in");
+				$("#projects").removeClass("in");
+	    });
 
-		$("#projectssvg").on("click", function() {
-			$("#aboutMe").removeClass("in");
-			$("#education").removeClass("in");
-			$("#experience").removeClass("in");		
-    });
+			$("#projectssvg").on("click", function() {
+				$("#aboutMe").removeClass("in");
+				$("#education").removeClass("in");
+				$("#experience").removeClass("in");		
+	    });
+
 
 });
